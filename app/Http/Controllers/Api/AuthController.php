56873438a9a1d9ccada529e74255405ecc19142c
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2021-09-25 01:20:20 +0300

    8. Documentation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -44,10 +44,11 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|confirmed|min:6',
+            'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
@@ -73,7 +74,6 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-
         return response()->json(['message' => 'User successfully signed out']);
     }
 
