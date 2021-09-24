672abacb9d9572fc3a3c5687fc5d84a9cd39702f
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2021-09-25 00:35:19 +0300

    7. JWT

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,20 @@ use Illuminate\Support\Facades\Route;
 //return $request->user();
 //});
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth',
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/post/{id}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
-Route::post('/post/{id}',[PostController::class,'update']);
-Route::post('/posts/{id}',[PostController::class,'destroy']);
+Route::post('/post/{id}', [PostController::class, 'update']);
+Route::post('/posts/{id}', [PostController::class, 'destroy']);
