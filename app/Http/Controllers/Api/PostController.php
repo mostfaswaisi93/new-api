3ec56b5d711a5a24ea1ccffe977e7fa4115cd245
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2021-09-24 16:12:43 +0300

    6. Install JWT

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -12,6 +14,7 @@ class PostController extends Controller
 
     public function index()
     {
+
         $posts = PostResource::collection(Post::get());
         return $this->apiResponse($posts, 'ok', 200);
     }
@@ -25,6 +28,70 @@ class PostController extends Controller
             return $this->apiResponse(new PostResource($post), 'ok', 200);
         }
         return $this->apiResponse(null, 'The post Not Found', 404);
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null, $validator->errors(), 400);
+        }
+
+        $post = Post::create($request->all());
+
+        if ($post) {
+            return $this->apiResponse(new PostResource($post), 'The post Save', 201);
+        }
+
+        return $this->apiResponse(null, 'The post Not Save', 400);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->apiResponse(null, $validator->errors(), 400);
+        }
+
+        $post = Post::find($id);
+
+        if (!$post) {
+            return $this->apiResponse(null, 'The post Not Found', 404);
+        }
+
+        $post->update($request->all());
+
+        if ($post) {
+            return $this->apiResponse(new PostResource($post), 'The post update', 201);
+        }
+
+    }
+
+    public function destroy($id)
+    {
+
+        $post = Post::find($id);
+
+        if (!$post) {
+            return $this->apiResponse(null, 'The post Not Found', 404);
+        }
+
+        $post->delete($id);
+
+        if ($post) {
+            return $this->apiResponse(null, 'The post deleted', 200);
+        }
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/post/{id}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::post('/post/{id}',[PostController::class,'update']);
+Route::post('/posts/{id}',[PostController::class,'destroy']);
